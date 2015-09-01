6530341445f4d0e9d714ec4e440b1de057e0fea0
Andy Situ <and.situ@gmail.com>
2015-09-01 15:32:51 -0700

    slight changes

--- files/scripts.js
+++ files/scripts.js
@@ -1,15 +1,20 @@
+// Page is for onload function for window
 var canvas,
 	ctx;
 
 window.addEventListener("load", function(e) {
-	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+	// Getting width & height for canvas
+	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+		height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+		canvas_heightratio = 0.90,
+		canvas_widthratio = 3 / 4;
+
 
 	canvas = document.createElement("canvas");
 	var canvas_div = document.getElementById("canvas_container");
 	canvas_div.appendChild(canvas);
-	canvas.width = Math.floor(width * 3 / 4);
-	canvas.height = Math.floor( height * 0.90);
+	canvas.width = Math.floor(width * canvas_widthratio);
+	canvas.height = Math.floor( height * canvas_heightratio);
 
 	ctx = canvas.getContext('2d');
 
