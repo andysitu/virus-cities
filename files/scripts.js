a68ce793db5b1bd566c3ea11ee16c032209e6ed8
Andy Situ <and.situ@gmail.com>
2015-09-01 16:50:07 -0700

    menu div ele & other modifications to script.js

--- files/scripts.js
+++ files/scripts.js
@@ -1,28 +1,35 @@
 // Page is for onload function for window
 var canvas,
-	ctx;
+	ctx,
+	menu;
 
 window.addEventListener("load", function(e) {
-	// Getting width & height for canvas
+	// Make canvas, set screen.width & height with screen.setWidthHeight,
+	// set ui.width & height with ui.setWidthHeight.
 	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
 		height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
 		canvas_heightratio = 0.95,
-		canvas_widthratio = 4/5;
+		canvas_widthratio = 4/5,
+		ui_widthratio = 1/5;
 
+	menu = document.getElementById("menu");
 
 	canvas = document.createElement("canvas");
-	var canvas_div = document.getElementById("canvas_container");
-	canvas_div.appendChild(canvas);
+	var container = document.getElementById("container");
+	container.insertBefore(canvas, menu);
 	width = Math.floor(width * canvas_widthratio);
 	height = Math.floor( height * canvas_heightratio);
 
 	screen.setWidthHeight(width, height);
-	ui.setWidthHeight(width, canvas.width, height);
+	ui.setWidthHeight(Math.floor(width * ui_widthratio), height);
 	canvas.width = width;
 	canvas.height = height;
+	canvas.id = "canvas";
+
+	ui.changeMenu();
 
 	ctx = canvas.getContext('2d');
 
 	ctx.fillStyle = 'red';
-	ctx.fillRect(0,0, screen.width, screen.height);
+	ctx.fillRect(0,0, width, height);
 });
