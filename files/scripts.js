b11ce823d25d44deaa9fc564b40adc9e85c7f368
Andy Situ <and.situ@gmail.com>
2015-09-01 16:07:43 -0700

    move canvas creation back to onload

--- files/scripts.js
+++ files/scripts.js
@@ -3,5 +3,26 @@ var canvas,
 	ctx;
 
 window.addEventListener("load", function(e) {
-	screen.makeCanvas();
+	// Getting width & height for canvas
+	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+		height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+		canvas_heightratio = 0.95,
+		canvas_widthratio = 4/5;
+
+
+	canvas = document.createElement("canvas");
+	var canvas_div = document.getElementById("canvas_container");
+	canvas_div.appendChild(canvas);
+	width = Math.floor(width * canvas_widthratio);
+	height = Math.floor( height * canvas_heightratio);
+
+	screen.setWidthHeight(width, height);
+	ui.setWidthHeight(width, canvas.width, height);
+	canvas.width = width;
+	canvas.height = height;
+
+	ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = 'red';
+	ctx.fillRect(0,0, screen.width, screen.height);
 });
