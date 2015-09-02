e6812ad9d2a2c6fdedad2769d9f03d03cbb94ddd
Andy Situ <and.situ@gmail.com>
2015-09-01 18:19:58 -0700

    screen - add yCenter & xCenter

--- files/screen.js
+++ files/screen.js
@@ -1,8 +1,13 @@
 var screen = {
 	width: 0,
 	height: 0,
+	xCenter: 0,
+	yCenter: 0,
 	setWidthHeight(w, h) {
 		this.width = w;
 		this.height = h;
+		this.xCenter = w/2;
+		this.yCenter = h/2;
+	},
 	}
 }
