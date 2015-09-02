a771512481e0ceeb18ba5cfb9e811a7d4235c72b
Andy Situ <and.situ@gmail.com>
2015-09-02 12:12:21 -0700

    makeMap - if map is too small, map fits to canvas

--- files/map.js
+++ files/map.js
@@ -1,14 +1,33 @@
 map = {
 	lenX: 10,
 	lenY: 10,
+	setLenX(value){
+		this.lenX = value;
+	},
+	setLenY(value){
+		this.lenY = value;
+	},
 	world: [],
 	getWorld() {
 		return this.world;
 	},
 	makeMap() {
+	// makes an array of arrays containing the data points for the
+	// map. If the given lens are too small, then it'll increase
+	// the size of the map until it fits the entire canvas.
 		var lenX = this.lenX,
 			lenY = this.lenY,
-			world = this.world;
+			world = this.world,
+			maxBlocks = display.getMaxBlocks();
+
+		if (lenX < maxBlocks[0]) {
+			this.setLenX(maxBlocks[0]);
+			lenX = this.lenX;
+		}
+		if (lenY < maxBlocks[1]) {
+			this.setLenY(maxBlocks[1]);
+			lenY = this.lenY;
+		}
 
 		for (var i = 0; i < lenX; i++) {
 			world.push([]);
