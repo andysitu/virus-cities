d75d497b63ff4e2744492f26b55ae013b05ca85a
Andy Situ <and.situ@gmail.com>
2015-09-03 15:44:54 -0700

    adding city count to Player

--- files/player.js
+++ files/player.js
@@ -1,14 +1,19 @@
 function Player() {
 	this.money = 20000;
+	this.count = 0;
 }
 
-Player.prototype.change = function(moneyValue) {
+// any changes to prototype will lead to change in
+// pstatus div changing.
+Player.prototype.change = function(moneyValue, count) {
 	this.money += moneyValue;
+	this.count += count;
 	this.dispInfo();
 };
 
 Player.prototype.dispInfo = function() {
 	pstatus.displayInfo({
-		"money": "$" +this.money
+		"money": "$" +this.money,
+		"# of cities": this.count
 	});
 };
