2927c89e467cdf2e06e5cebb31b1832f0cda74b8
Andy Situ <and.situ@gmail.com>
2015-09-03 14:54:17 -0700

    pstatus added methods
    * similar from ui

--- files/pstatus.js
+++ files/pstatus.js
@@ -1,3 +1,5 @@
+// There is an intentional link between Player and pstatus.
+
 pstatus = {
 	width: 0,
 	height: 0,
@@ -6,5 +8,22 @@ pstatus = {
 		this.height = height;
 		status_display.style.width = width + "px";
 		status_display.style.height = height + "px";
+	},
+	display(str) {
+		status_display.textContent = str;
+	},
+	displayInfo(obj) {
+		var str = this.selectedInfo(obj);
+
+		this.display(str);
+	},
+	selectedInfo(obj) {
+		var str = "";
+
+		for (var key in obj) {
+			str += " " + key + ": " + obj[key] + "\n";
+		}
+
+		return str;
 	}
 }
