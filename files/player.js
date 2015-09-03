f51bf67cb941ea28eb1956a0a4261453895495ac
Andy Situ <and.situ@gmail.com>
2015-09-03 14:55:00 -0700

    player methods (change displays pstatus)

--- files/player.js
+++ files/player.js
@@ -1,3 +1,14 @@
 function Player() {
 	this.money = 20000;
 }
+
+Player.prototype.change = function(moneyValue) {
+	this.money += moneyValue;
+	this.dispInfo();
+};
+
+Player.prototype.dispInfo = function() {
+	pstatus.displayInfo({
+		"money": "$" +this.money
+	});
+};
