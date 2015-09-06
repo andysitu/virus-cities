8baf8bc2d911fe17d57154e70376aa1bd7ed13cd
Andy Situ <and.situ@gmail.com>
2015-09-05 18:03:50 -0700

    settlement prop, run(), calculatePop()

--- files/settlement.js
+++ files/settlement.js
@@ -3,7 +3,13 @@ function Settlement(inherited) {
 	this.inherited = inherited;
 	this.id = "settlement";
 	inheritLand(this, inherited);
-	
+	this.population = 100 + Math.round(Math.random() * 400);
+	this.employed = 0;
+	this.infected = 0;
+	this.profit = 0;
+	this.treasure = 0;
+	this.food = 1;
+
 	// This will be persistence in all instances (even inherited);
 	this.type = "settlement";
 }
@@ -15,5 +21,15 @@ Settlement.prototype.click = function() {
 		livestock: this.livestock,
 		resistance: this.resistance,
 		"x, y": [this.x, this.y],
+		population: this.population,
+		food: this.food
 	};
+};
+
+Settlement.prototype.calculatePop = function() {
+	this.population += Math.floor( this.food / 100 * (this.population - this.infected) );
+};
+
+Settlement.prototype.run = function() {
+	this.calculatePop();
 };
